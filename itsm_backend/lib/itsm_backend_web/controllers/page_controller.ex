883828f97964ee330a553efe241b47b1d82f1851
Lu <66143267+TolulopeVerissimo@users.noreply.github.com>
2025-09-26 13:41:12 -0400

    create phoenix w/postgres

---
+++ itsm_backend/lib/itsm_backend_web/controllers/page_controller.ex
@@ -0,0 +1,7 @@
+defmodule ItsmBackendWeb.PageController do
+  use ItsmBackendWeb, :controller
+
+  def home(conn, _params) do
+    render(conn, :home)
+  end
+end
---
+++ itsm_backend/test/itsm_backend_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule ItsmBackendWeb.PageControllerTest do
+  use ItsmBackendWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end

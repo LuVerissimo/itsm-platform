4971aef5f3628749bf8c7d0aadc9df9521409bda
Lu <66143267+TolulopeVerissimo@users.noreply.github.com>
2025-09-30 14:52:42 -0400

    only admins allowed!

---
+++ itsm_backend/lib/itsm_backend_web/plugs/admin_auth_plug.ex
@@ -0,0 +1,19 @@
+defmodule ItsmBackendWeb.AdminAuthPlug do
+
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    user_id = get_session(conn, :user_id)
+    user = user_id && Accounts.get_user!(user_id)
+
+    if user && user.role == "admin" do
+      conn
+    else
+      conn
+      |> put_status(:forbidden)
+      |> Phoenix.Controller.json(%{error, "Admin access required"})
+      |> halt()
+    end
+  end
+end

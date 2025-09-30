6658e7a315cc92be1d8a58f9dee5dede1003ed57
Lu <66143267+TolulopeVerissimo@users.noreply.github.com>
2025-09-30 14:36:00 -0400

    create plug to check session

---
+++ itsm_backend/lib/itsm_backend_web/plugs/auth_plug.ex
@@ -0,0 +1,17 @@
+defmodule ItsmBackendWeb.AuthPlug do
+
+  import Plug.Conn
+
+  def init(ops), do: opts
+
+  def call(conn, _opts) do
+    if get_session(conn, :user_id) do
+      conn
+    else
+      conn
+      |> put_status(:unauthorized)
+      |> Phoenix.Controller.json(%{error: "Authentication required"})
+      |> halt()
+    end
+  end
+end
